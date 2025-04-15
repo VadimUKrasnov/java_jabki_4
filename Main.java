d60a936d55edc0899c2f8cd16e8bb13816b3abee
Vadim Krasnov <wadsonred@gmail.com>
2025-04-15 13:55:51 +0300

    java_jabki_4

--- Main.java
+++ Main.java
@@ -7,6 +7,32 @@ public class Main {
 
         int factorial = factorial(3);
         System.out.println(factorial);
+
+        // 1.
+        System.out.println(sumRange(3, 6));
+
+        // 2.
+        System.out.println(add(5, 7));
+        System.out.println(add("жызнь ", "боль"));
+
+
+        // 3.
+        int[] array = {3, 5, 7, 2, 8, -1, 4};
+        System.out.println("Максимальный элемент в массиве: " + findMax(array));
+
+        // 4.
+        System.out.println("Факториал числа:" + " " + factorialN(4));
+
+        // 5.
+        System.out.println("Площадь круга: " + getArea(5));
+        System.out.println("Площадь прямоугольника: " + getArea(4, 3));
+
+        // 6.
+        System.out.println("Cреднее арифметическое: " + getAverage(6, 4, 7));
+
+        // 7.
+        System.out.println("Длина гипотенузы: " + calculateHypotenuse(5, 4));
+
     }
 
     /**
@@ -59,4 +85,64 @@ public class Main {
      *     -> 3 * 2
      * -> 6
      */
+
+    /* 1. Создайте метод sumRange, который принимает два числа (начало и конец диапазона)
+     и возвращает сумму всех чисел между ними (включительно).
+     */
+    public static int sumRange(int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    // 2. Создайте два перегруженных метода add: Первый складывает два целых числа; Второй складывает две строки.
+    public static int add(int a, int b) {
+        return a+b;
+    }
+    public static String add(String a, String b) {
+        return a+b;
+    }
+
+    // 3. Напишите метод findMax, который принимает массив целых чисел и возвращает его максимальный элемент.
+    public static int findMax(int[] numbers) {
+        int max = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        return max;
+    }
+
+    // 4. Напишите метод, вычисляющий факториал числа с помощью цикла for
+    public static long factorialN(int n) {
+        long result = 1;
+        if (n < 0) {
+            throw new IllegalArgumentException("Число должно быть неотрицательным");
+        }
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    // 5. Создайте перегруженные методы calculateArea: Для круга (принимает радиус); Для прямоугольника (принимает длину и ширину).
+    public static double getArea(int r) {
+        return Math.round(Math.PI * r * r) * 100 / 100.0;
+    }
+    public static int getArea(int a, int b) {
+        return a * b;
+    }
+
+    // 6. Реализуйте метод, который принимает три числа и возвращает их среднее арифметическое.
+    public static double getAverage(int a, int b, int c) {
+        return Math.round((a + b + c) * 100.0 / 3) / 100.0;
+    }
+
+    // 7. Напишите метод, который принимает два числа, представляющие длины катетов, и возвращает длину гипотенузы (используйте теорему Пифагора).
+    public static double calculateHypotenuse(int a, int b) {
+        return Math.round(Math.sqrt((a * a) + (b * b)) * 100) / 100.0;
+    }
 }
--- MainTest.java
+++ MainTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MainTest {
 
@@ -15,5 +15,81 @@ class MainTest {
     void multiply() {
         int a = 2, b = 3;
         Assertions.assertEquals(a * b, Main.multiply(a, b));
+    }
+
+    @Test
+    void testHello() {
+    }
+
+    @Test
+    void testMultiply() {
+    }
+
+    // ДЗ
+    @Test
+    void sumRange() {
+        int a = 7;
+        int b = 9;
+        Assertions.assertEquals(24, Main.sumRange(a, b));
+    }
+
+    @Test
+    void addFirst() {
+        int a = 1;
+        int b = 9;
+        Assertions.assertEquals(10, Main.add(a, b));
+    }
+
+    @Test
+    void addSecond() {
+        String a = "Добрый ";
+        String b = "день!";
+        Assertions.assertEquals("Добрый день!", Main.add(a, b));
+    }
+
+    @Test
+    void findMax() {
+        int [] a = {-1, 2, 3, 10, -8, 0};
+        Assertions.assertEquals(10, Main.findMax(a));
+    }
+
+    @Test
+    void calculateFactorial() {
+        int a = 3;
+        Assertions.assertEquals(6, Main.factorialN(a));
+    }
+
+    @Test
+    void calculateFactorialSecond() {
+        int a = -2;
+        assertThrows(IllegalArgumentException.class, () -> Main.factorialN(a));
+    }
+
+    @Test
+    void getAreaFirst() {
+        int a = 3;
+        Assertions.assertEquals(28, Main.getArea(a));
+    }
+
+    @Test
+    void testGetAreaSecond() {
+        int a = 3;
+        int b = 3;
+        Assertions.assertEquals(9, Main.getArea(a, b));
+    }
+
+    @Test
+    void getAverage() {
+        int a = 3;
+        int b = 4;
+        int c = 5;
+        Assertions.assertEquals(4, Main.getAverage(a, b, c));
+    }
+
+    @Test
+    void calculateHypotenuse() {
+        int a = 3;
+        int b = 4;
+        Assertions.assertEquals(5, Main.calculateHypotenuse(a, b));
     }
 }
